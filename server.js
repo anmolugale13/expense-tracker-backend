f04cd7255c1fb6016e3c26787ea1390e291c2508
anmolugale13 <anmolugale13@gmail.com>
2025-09-14 23:10:23 +0530

    Use dynamic PORT for Render deployment

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use('/api/expenses', expenseRoutes);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('✅ MongoDB connected');
-    app.listen(5000, () => console.log('🚀 Server running on port 5000'));
+    const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
   })
   .catch(err => console.error(err));
